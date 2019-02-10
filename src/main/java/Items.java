4e3a18f0bee2b328baff3bc55c00ce45e07d616d
abdul <abdulgmohd@gmail.com>
2019-02-09 20:26:03 -0800

    fix

--- src/main/java/Items.java
+++ src/main/java/Items.java
@@ -2,7 +2,7 @@ public class Items {
 
     public static void printInfo(){
         String userId = System.getProperty("FTP_USER");
-        String pw = System.getenv("FTP_PASSWORD");
+        String pw = System.getProperty("FTP_PASSWORD");
         System.out.println(userId);
         System.out.println(pw);
     }
