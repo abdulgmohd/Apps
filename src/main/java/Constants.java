36653f10d4a53ff1a177a92900300053008f7d87
abdul <abdulgmohd@gmail.com>
2019-02-11 19:01:38 -0800

    adding constants

---
+++ src/main/java/Constants.java
@@ -0,0 +1,5 @@
+public class Constants {
+
+    public static String FTP_USER = System.getenv("FTP_USER");
+    public static String FTP_PASSWORD=  System.getenv("FTP_PASSWORD");
+}
--- src/main/java/Items.java
+++ src/main/java/Items.java
@@ -1,13 +1,13 @@
 public class Items {
 
     public String getFtpUser(){
-        String userId = System.getenv("FTP_USER");
+        String userId = Constants.FTP_USER;
         return userId;
     }
 
 
     public String getFtpPassword(){
-        String pw = System.getenv("FTP_PASSWORD");
+        String pw = Constants.FTP_PASSWORD;
         return pw;
     }
 
