5a3826dc09a645f34b35138b80f8b58a0e147089
abdul <abdulgmohd@gmail.com>
2019-02-24 18:05:47 -0800

    fix

--- src/test/java/ItemsTest.java
+++ src/test/java/ItemsTest.java
@@ -2,9 +2,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ItemsTest {
+
+    public static  String[] card = Constants.VISA_CARD.split(",");
     @Test
-    public void getCCNumber(String[] creditCard) throws Exception {
-        String[] card = Constants.VISA_CARD.split(",");
+    public void getCCNumber() throws Exception {
        String cardNumber = card[0];
        Assert.assertEquals("4444444444444448", cardNumber);
     }
